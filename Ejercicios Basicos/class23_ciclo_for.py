d833068ba028870d9026a7795316650c1d119b28
Renato DarkGoshLap <darkgoshmx@gmail.com>
2023-03-20 21:32:54 -0600

    Class 24 For_Recorrer /

--- Ejercicios Basicos/class23_ciclo_for.py
+++ Ejercicios Basicos/class23_ciclo_for.py
@@ -7,27 +7,25 @@ V1.0
 Date: 20/03/2023
 """
 
-#print('esta es una prueba')
-
-contador = 0
-#print(contador)
-while contador < 10:
-    contador += 1 # esto es igual a ' contador = contador + 1 '
-    print(contador)
-
-
-"""
-a = list(range(1001)) #imprime una lista del 0 al 1000
-print(a)
+#ejemplo de un contador que continue hasta llegar a 100 usando while
+# contador = 0
+# #print(contador)
+# while contador < 100:
+#     contador += 1 # Esto es igual a: ' contador = contador + 1 '
+#     print(contador)
 
 
-for contador in range(1,1001):  #imprime los numeros del 1 al 1000
-    print(contador)
+# a = list(range(1001)) #imprime una lista del 0 al 1000
+# print(a)
 
-for i in range(0,10): #imprime la tabla del 11
-    print(11*i)
+
+# for contador in range(1,1001):  #imprime los numeros del 1 al 1000
+#     print(contador)
+
+# for i in range(0,10): #imprime la tabla del 11
+#         print(11*i)
 
 
 for i in range(0,11): #imprime la tabla del 7
     print(7*i)
-"""
+
