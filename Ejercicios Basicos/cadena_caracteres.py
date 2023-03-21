17ce861374352d371bf62bc31ae4882b9ed0464c
Renato DarkGoshLap <darkgoshmx@gmail.com>
2023-03-20 19:53:36 -0600

    Class 18 METODOS con cadena de caracteres/ Agregue mas metodos

--- Ejercicios Basicos/cadena_caracteres.py
+++ Ejercicios Basicos/cadena_caracteres.py
@@ -9,5 +9,10 @@ Date: 20/03/2023
 
 nombre = input("¿Cual es tu nombre?:  ")
 print(nombre)
-
-
+print(nombre.upper()) #cambia todas las letras a mayusculas
+print(nombre.capitalize()) # Solo cambia la primera letra a Mayusculas
+print(nombre.strip()) # quita los espacios
+print(nombre.lower()) #cambia todas las letras a Minusculas
+print(nombre.replace('o', 'a')) # Cambia las letras ' o ' por letras ' a '
+print(nombre[0])# uso de INDICES python devuelve la letra segun la posicion que de coloque entre corchetes [0]
+print(len("renato vidana vazquez")) #cuenta el numero de caracteres contenido en el texto. incluyendo los espacios.
