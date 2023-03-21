064a67b23dd80a355d43ff0d6eb1abe1e8e560d0
Renato DarkGoshLap <darkgoshmx@gmail.com>
2023-03-21 17:29:39 -0600

    class Listas /

---
+++ Ejercicios Basicos/class28_Listas.py
@@ -0,0 +1,14 @@
+"""
+Curso Basico de Python
+Clase No.28 Uso de listas
+@Platzi
+@Darkgosh
+V1.0
+Date: 21/03/2023
+"""
+
+def run():
+    objetos = ['Hola', 3, 4.5, True]
+
+if __name__ =='__main__':
+    run()
