8b4c303d9c445572c108720d319021ec91aec0bb
Renato DarkGoshLap <darkgoshmx@gmail.com>
2023-03-21 11:23:43 -0600

    Class 25 Break and Continue / Add Comments

--- Ejercicios Basicos/class25_break_continue.py
+++ Ejercicios Basicos/class25_break_continue.py
@@ -8,9 +8,10 @@ Date: 21/03/2023
 """
 
 def run():
-    # for contador in range (1000):
-    #     if contador % 2 != 0:
-    #         continue
+    # for contador in range (100): 
+    #     if contador % 2 != 0: # si el contador Modulo 2 (al dividirlo por 2) es distinto de 0 
+    #         #(que la division no tenga residuos osease el residuo debe ser 0)
+    #         continue # si no cumple con la condicion lo que esta debajo del Continue no se va ejecutar
     #     print(contador)
 
     # for i in range(10000):
@@ -18,13 +19,13 @@ def run():
     #     if i == 5678:
     #         break
 
-    # texto = input('Escribe un texto: ')
-    # for letra in texto:
-    #     if letra == 'o':
-    #         break
-    #     if letra == 'O':
-    #         break
-    #     print(letra)
+    texto = input('Escribe un texto: ')
+    for letra in texto:
+        if letra == 'o':
+            break
+        if letra == 'O':
+            break
+        print(letra)
 
     # contador = 1
     # print(contador)
@@ -34,13 +35,13 @@ def run():
     #         break
     #     print(contador)
 
-    contador = 1
-    print(contador)
-    while contador < 100:
-        contador = contador +1
-        if contador % 2 != 0:
-            break
-        print(contador)
+    # contador = 1
+    # print(contador)
+    # while contador < 100:
+    #     contador = contador +1
+    #     if contador % 2 != 0:
+    #         break
+    #     print(contador)
 
 
 if __name__ == '__main__':
