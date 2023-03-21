08eb81b4c8d48a877360c8b2831632a4a943eec0
Renato DarkGoshLap <darkgoshmx@gmail.com>
2023-03-21 17:54:34 -0600

    class tuplas /

--- Ejercicios Basicos/class28_Listas.py
+++ Ejercicios Basicos/class28_Listas.py
@@ -8,7 +8,13 @@ Date: 21/03/2023
 """
 
 def run():
-    objetos = ['Hola', 3, 4.5, True]
+    objetos = ['Hola', 3, 4.5, True, 1]
+    for elementos in objetos:
+        print(elementos)
+    print(objetos[::-1])
+    print(objetos[1:3])
 
+    # newElemento = input('Escribre un nuevo elemento: ')
+    # newElemento = objetos.append(newElemento)
 if __name__ =='__main__':
     run()
