e6a4a516461f3c35aca737f926c5038a2e9ccf3e
Renato DarkGoshLap <darkgoshmx@gmail.com>
2023-04-21 21:32:04 -0600

    new File Game Piedra-Papel-Tijera

--- CursoBasicoPython/class29_tuplas.py
+++ CursoBasicoPython/class29_tuplas.py
@@ -7,6 +7,8 @@ V1.0
 Date: 21/03/2023
 """
 
+#Las TUPLAS son solo lectura y hacer una DECLARACION. No se pueden crear, ni actualizaar, ni borrar.
+
 def run():
     numeros = [1, 2 ,3, 4, 5]
     print(numeros)
@@ -27,3 +29,35 @@ def run():
 
 if __name__ =='__main__':
     run()
+
+
+numbers = (1,2,3,5) #declaracion
+strings = ('nico','zule','santi')
+print(numbers)
+print('0 =>',numbers[0])
+print('- 1', numbers[-1])
+print(type(numbers))
+      
+print(strings)
+print(type(strings))
+
+#LISTAS SON CRUD=(crear,read,update, delete)
+
+
+#Convertir una TUPLA a una LISTA
+my_list = list(strings)
+print(my_list)
+print(type(my_list))
+
+my_list[1] = 'juli'
+print(my_list)
+
+#Convertir una LISTA a una TUPLA
+my_tuple = tuple(my_list)
+print(my_tuple)
+
+
+
+
+  
+
