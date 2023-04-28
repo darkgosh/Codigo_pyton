9fd923d9c4e05e5cccd59fcd4fe411c747424683
Renato DarkGoshLap <darkgoshmx@gmail.com>
2023-04-28 17:58:00 -0600

    Cambio de directorios

--- RandomWebExcesices/IO_World_Excel/IO_World_Excel.py
+++
@@ -1,5 +0,0 @@
-import os
-import shutil
-import pandas
-from docxtpl import DocxTemplate, Inlineimage
-from docx.shared import 
---
+++ RandomWebExcesices/IO_World_Excel/Inputs/IO_World_Excel.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+import pandas
+from docxtpl import DocxTemplate, Inlineimage
+from docx.shared import Mm
+
+
+############# Configuracion de Usuario ##############
+
+# Ruta de Salida
+OUTPUT_PATH = '.\Outputs'
+
+#Ruta fichero Excel
+EXCEL_PATH = '.\Inputs\People_Data.xlsx'
+
+#Ruta ficheros plantillas de Word
+ES_WORD_TPL_PATH = '.\Inputs\Templates\WordTeplate_ES.docx'
+EN_WORD_TPL_PATH = '.\Inputs\Templates\WordTeplate_EN.docx'
+
+#Ruta de Imagenes
+IMAGE_PATH = '.\Inputs\Images'
+
+############# Configuracion de Usuario ##############
+
