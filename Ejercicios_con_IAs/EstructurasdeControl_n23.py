184306c13e656fe110432ba6f70bf1f6820afc02
Renato DarkGoshLap <darkgoshmx@gmail.com>
2024-06-21 23:12:48 -0600

    New Practice Files 16 to 25 on Curso_Python with IAs

---
+++ Ejercicios_con_IAs/EstructurasdeControl_n23.py
@@ -0,0 +1,13 @@
+# @darkgosh Ejercicio proporcionados por IAs
+# Practica de Estructuras de control
+# Date....
+
+'''
+Descripcion delejercicio......
+'''
+#solicita datos
+
+#Estructura de control
+
+#imprime mensaje
+
