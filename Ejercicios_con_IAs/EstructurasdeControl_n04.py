95e40d569c0b3ed0b215e319f9f5ee746daca4cf
Renato DarkGoshLap <darkgoshmx@gmail.com>
2024-06-21 21:26:00 -0600

    Update EstructurasdeControl_n04.py on Curso_Python with IAs

--- Ejercicios_con_IAs/EstructurasdeControl_n04.py
+++ Ejercicios_con_IAs/EstructurasdeControl_n04.py
@@ -3,11 +3,22 @@
 # Date....
 
 '''
-Descripcion delejercicio......
+Clasificación de edades
+Escribe un programa que solicite al usuario su edad y muestre un mensaje en función del rango de edad:
+Niño: menor de 12 años
+Adolescente: entre 12 y 18 años
+Adulto: entre 18 y 65 años
+Adulto mayor: mayor de 65 años
+Utiliza la estructura if-elif-else para clasificar la edad del usuario.
 '''
 #solicita datos
+edad= int(input("Escribe tu edad: "))
 
-#Estructura de control
-
-#imprime mensaje
+#estructura de control
+if edad <= 12:
+  print(f"Con {edad} años, Eres un Niño" )
+elif edad > 12 and edad <=17:
+  print(f"Con {edad} años, Eres un Adolesente" )
+else:
+  print(f"Con {edad} años, Eres un Adulto" )
 
