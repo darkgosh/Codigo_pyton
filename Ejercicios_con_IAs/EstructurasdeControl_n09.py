9d91f6e35f9d35eaebde07cb7d36bbbc3359fee4
Renato DarkGoshLap <darkgoshmx@gmail.com>
2024-06-27 02:14:11 -0600

    Update estructurasdeControln09 (ImprimirTriangulo)

--- Ejercicios_con_IAs/EstructurasdeControl_n09.py
+++ Ejercicios_con_IAs/EstructurasdeControl_n09.py
@@ -5,13 +5,18 @@
 '''
 Patrones de asteriscos
 
-Crea un programa que imprima patrones de asteriscos en forma de pirámide, triángulo invertido, diamante, etc., según la altura o número de filas que ingrese el usuario.
+Crea un programa que imprima patrones de asteriscos en forma de pirámide, triángulo invertido, diamante, etc., 
+según la altura o número de filas que ingrese el usuario.
 
 Utiliza ciclos for anidados para controlar la posición de los asteriscos en cada fila y columna.
 '''
 #solicita datos
+n = int(input("Dame un numero entero: "))
 
-
+for i in range (n):
+    for j in range(i+1):
+        print("^", end="")
+    print("")
 #Estructura de control
 
 
