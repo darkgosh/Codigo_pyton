bb96098dd22f2dcfbb9d8d7fd38ca44a02de5912
Renato DarkGoshLap <darkgoshmx@gmail.com>
2024-07-01 23:56:41 -0600

    Update EstructurasdeControl_n10.py

--- Ejercicios_con_IAs/EstructurasdeControl_n10.py
+++ Ejercicios_con_IAs/EstructurasdeControl_n10.py
@@ -13,6 +13,41 @@ Estos ejercicios te ayudarán a practicar el uso de diferentes estructuras de co
 como if-elif-else, while, for y switch, para controlar el flujo de ejecución de tu código y realizar tareas diversas.
 '''
 #solicita datos
+def contar_caracteres(texto):
+  """
+  Esta función cuenta la cantidad de letras, dígitos y espacios en blanco en un texto ingresado por el usuario.
+
+  Argumentos:
+    texto (str): El texto que se desea analizar.
+
+  Retorna:
+    dict: Un diccionario con las siguientes claves:
+      "letras": Cantidad de letras en el texto.
+      "digitos": Cantidad de dígitos en el texto.
+      "espacios": Cantidad de espacios en blanco en el texto.
+  """
+  conteo_caracteres = {"letras": 0, "digitos": 0, "espacios": 0}
+
+  for caracter in texto:
+    if caracter.isalpha():
+      conteo_caracteres["letras"] += 1
+    elif caracter.isdigit():
+      conteo_caracteres["digitos"] += 1
+    else:
+      conteo_caracteres["espacios"] += 1
+
+  return conteo_caracteres
+
+# Solicitar al usuario el texto
+texto_usuario = input("Ingrese un texto: ")
+
+# Contar los caracteres
+conteo_caracteres = contar_caracteres(texto_usuario)
+
+# Mostrar el conteo de caracteres
+print("Cantidad de letras:", conteo_caracteres["letras"])
+print("Cantidad de dígitos:", conteo_caracteres["digitos"])
+print("Cantidad de espacios:", conteo_caracteres["espacios"])
 
 
 #Estructura de control
