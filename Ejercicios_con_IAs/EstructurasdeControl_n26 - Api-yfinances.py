ab4895c108eab2bb88c2d55e9adc52b029e7863f
Ren Gosh <darkgoshmx@gmail.com>
2024-07-03 15:50:27 -0600

    New file estructuras de python_25-API-yfianances.py

---
+++ Ejercicios_con_IAs/EstructurasdeControl_n26 - Api-yfinances.py
@@ -0,0 +1,23 @@
+# @darkgosh Ejercicio proporcionados por https://aprendeconalf.es/
+# Practica de Estructuras de control
+# Date....
+
+'''
+Escribir un programa que pida al usuario un número entero 
+y muestre por pantalla un triángulo rectángulo como el de más abajo.
+1
+3 1
+5 3 1
+7 5 3 1
+9 7 5 3 1
+'''
+#solicita datos
+
+
+
+#Estructura de control
+
+
+
+#imprime mensaje
+
