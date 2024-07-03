74e5d4ac52f23f67de00ab4f5153bb9f7a3ad26b
Ren Gosh <darkgoshmx@gmail.com>
2024-07-03 16:20:07 -0600

    update ADDED aditional if on EstructurasdeControl_n10.py

--- Ejercicios_con_IAs/EstructurasdeControl_n10.py
+++ Ejercicios_con_IAs/EstructurasdeControl_n10.py
@@ -11,6 +11,7 @@ y cuente la cantidad de letras, dígitos y espacios en blanco que contiene.
 Utiliza un ciclo for para recorrer cada carácter del texto y verificar su tipo 
 (letra, dígito o espacio).
 
+tambien agregar si es minuscula o es mayuscula caracter.isupper() y caracter.islower().
 Estos ejercicios te ayudarán a practicar el uso de diferentes estructuras de control en Python, 
 como if-elif-else, while, for y switch, para controlar el flujo de ejecución de tu código y realizar tareas diversas.
 '''
@@ -28,11 +29,15 @@ def contar_caracteres(texto):
       "digitos": Cantidad de dígitos en el texto.
       "espacios": Cantidad de espacios en blanco en el texto.
   """
-  conteo_caracteres = {"letras": 0, "digitos": 0, "espacios": 0}
+  conteo_caracteres = {"letras": 0, "digitos": 0, "espacios": 0, "mayusculas":0, "minusculas":0}
 
   for caracter in texto:
     if caracter.isalpha():
       conteo_caracteres["letras"] += 1
+      if caracter.isupper():
+        conteo_caracteres["mayusculas"] += 1
+      else:
+        conteo_caracteres["minusculas"] += 1
     elif caracter.isdigit():
       conteo_caracteres["digitos"] += 1
     else:
@@ -50,8 +55,8 @@ conteo_caracteres = contar_caracteres(texto_usuario)
 print("Cantidad de letras:", conteo_caracteres["letras"])
 print("Cantidad de dígitos:", conteo_caracteres["digitos"])
 print("Cantidad de espacios:", conteo_caracteres["espacios"])
-
-
+print("Cantidad de Minusculas:", conteo_caracteres["minusculas"])
+print("Cantidad de Mausculas:", conteo_caracteres["mayusculas"])
 #Estructura de control
 
 
