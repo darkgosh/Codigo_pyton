6111e48f4502ccbfc849c57a14bcf54e2d7b4180
Ren Gosh <darkgoshmx@gmail.com>
2024-07-03 16:01:18 -0600

    update EstructurasdeControl_n10.py

--- Ejercicios_con_IAs/EstructurasdeControl_n10.py
+++ Ejercicios_con_IAs/EstructurasdeControl_n10.py
@@ -5,9 +5,11 @@
 '''
 Análisis de texto
 
-Escribe un programa que lea un texto ingresado por el usuario y cuente la cantidad de letras, dígitos y espacios en blanco que contiene.
+Escribe un programa que lea un texto ingresado por el usuario 
+y cuente la cantidad de letras, dígitos y espacios en blanco que contiene.
 
-Utiliza un ciclo for para recorrer cada carácter del texto y verificar su tipo (letra, dígito o espacio).
+Utiliza un ciclo for para recorrer cada carácter del texto y verificar su tipo 
+(letra, dígito o espacio).
 
 Estos ejercicios te ayudarán a practicar el uso de diferentes estructuras de control en Python, 
 como if-elif-else, while, for y switch, para controlar el flujo de ejecución de tu código y realizar tareas diversas.
