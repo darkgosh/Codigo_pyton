1ddb3b73ec2e6b890b7c9223d7bf26b5adc72e08
Renato DarkGoshLap <darkgoshmx@gmail.com>
2024-07-08 01:22:28 -0600

    Update EstructurasdeControl_n21-Calculo_Intereses.py

--- Ejercicios_con_IAs/EstructurasdeControl_n22-Calculo_Intereses.py
+++ Ejercicios_con_IAs/EstructurasdeControl_n22-Calculo_Intereses.py
@@ -7,7 +7,8 @@ Escribir un programa que pregunte al usuario una cantidad a invertir, el interé
 y el número de años, y muestre por pantalla el capital obtenido en la inversión cada año que dura la inversión.
 '''
 #solicita datos
-
+inv_inicial = float(input("Ingresa la cantida a Invertir: "))
+tasa = 0
 
 #Estructura de control
 
