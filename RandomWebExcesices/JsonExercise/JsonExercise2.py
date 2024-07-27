41b926276108c3c2fa239b0d69dc1f94bcdd0545
Renato DarkGoshLap <darkgoshmx@gmail.com>
2024-07-26 21:27:30 -0600

    New Json Exercises2

--- RandomWebExcesices/JsonExercise/JsonExercise2.py
+++ RandomWebExcesices/JsonExercise/JsonExercise2.py
@@ -1,9 +1,10 @@
 '''Exercise with json strings ' loads ',' dumps  '
 '''
 import json
-#import os
-#print(os.getcwd())
-#with open(r"g:\My Drive\Software_Projects\PROYECTOS\Curso_Python\RandomWebExcesices\JsonExercise\data.json", "r") as f:
-with open("data.json","r") as f:
+
+with open("g:/My Drive/Software_Projects/PROYECTOS/Curso_Python/RandomWebExcesices/JsonExercise/data.json","r") as f:
     data = json.load(f) 
-print(data)
+print(data.items())
+
+with open("g:/My Drive/Software_Projects/PROYECTOS/Curso_Python/RandomWebExcesices/JsonExercise/data2.json","w") as f:
+    json.dump(data,f) 
